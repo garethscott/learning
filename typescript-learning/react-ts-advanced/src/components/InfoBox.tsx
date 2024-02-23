535b28c7ebd13e0b97d1ec675a84356a4e5fefaf
Gareth <garethscott@1plus1loans.co.uk>
2024-02-23 14:32:43 +0000

    added InfoBox

--- typescript-learning/react-ts-advanced/src/components/InfoBox.tsx
+++ typescript-learning/react-ts-advanced/src/components/InfoBox.tsx
@@ -2,10 +2,13 @@ import { type ReactNode } from "react";
 
 interface InfoBoxProps {
   mode: "hint" | "warning";
+  // If you have a component with a prop that isnt always required you can add a question mark after
+  // the prop in the interface and it will be regarded as undefined, and the error will go away
+  severity?: "low" | "medium" | "high";
   children: ReactNode;
 }
 
-export default function InfoBox({ mode, children }: InfoBoxProps) {
+export default function InfoBox({ mode, severity, children }: InfoBoxProps) {
   if (mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
@@ -15,7 +18,7 @@ export default function InfoBox({ mode, children }: InfoBoxProps) {
   }
 
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
