6659cbe85a8e240328ee10fc09ea782e90b0c9f5
Gareth <garethscott@1plus1loans.co.uk>
2024-02-27 08:58:23 +0000

    Finished goals project, set up new project directory

--- typescript-learning/react-ts-advanced/src/components/InfoBox.tsx
+++ typescript-learning/react-ts-advanced/src/components/InfoBox.tsx
@@ -1,14 +1,31 @@
 import { type ReactNode } from "react";
 
-interface InfoBoxProps {
-  mode: "hint" | "warning";
-  // If you have a component with a prop that isnt always required you can add a question mark after
-  // the prop in the interface and it will be regarded as undefined, and the error will go away
-  severity?: "low" | "medium" | "high";
-  children: ReactNode;
-}
+// interface InfoBoxProps {
+//   mode: "hint" | "warning";
+//   // If you have a component with a prop that isnt always required you can add a question mark after
+//   // the prop in the interface and it will be regarded as undefined, and the error will go away
+//   severity?: "low" | "medium" | "high";
+//   children: ReactNode;
+// }
 
-export default function InfoBox({ mode, severity, children }: InfoBoxProps) {
+// Below is an example of using discriminate unions within a component, condition on using two different
+// sets of props based on the mode prop 'hint' or 'warning'
+type HintBoxProps = {
+  mode: "hint";
+  children: ReactNode;
+};
+
+type WarningBoxProps = {
+  mode: "warning";
+  severity: "low" | "medium" | "high";
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { children, mode } = props;
+
   if (mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
@@ -16,6 +33,8 @@ export default function InfoBox({ mode, severity, children }: InfoBoxProps) {
       </aside>
     );
   }
+
+  const { severity } = props;
 
   return (
     <aside className={`infobox infobox-warning warning--${severity}`}>
