936bee367dfdf648ed2becfbd97e4257e51b484a
Gareth <garethscott@1plus1loans.co.uk>
2024-03-28 17:42:22 +0000

    more changes to Form component

--- typescript-learning/ts-project-04/src/components/Form.tsx
+++ typescript-learning/ts-project-04/src/components/Form.tsx
@@ -1,7 +1,14 @@
-import { type ComponentPropsWithoutRef } from 'react';
+import { type FormEvent, type ComponentPropsWithoutRef } from 'react';
 
 type FormProps = ComponentPropsWithoutRef<'form'>;
 
 export default function Form(props: FormProps) {
+  // function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+
+  //   const formData = new FormData(event.currentTarget);
+  //   const data = Object.fromEntries(formData);
+  // }
+
   return <form {...props}>{props.children}</form>;
 }
